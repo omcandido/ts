465d3de9bab0a020fc507635c46b87db5dd5f0c7
Cándido Otero Moreira <omcandido@gmail.com>
2024-07-19 22:01:09 +0000

    add example of how to build your codebase

---
+++ src/utils.py
@@ -0,0 +1,8 @@
+# Python module containing auxiliary functions
+
+# Returns the N first fibonnaic numbers
+def fibonacci(n):
+    fib = [0, 1]
+    for i in range(2, n):
+        fib.append(fib[i-1] + fib[i-2])
+    return fib
