2eeb9d594813b010ae73e833ea2cfcd772762232
Cándido Otero <omcandido@gmail.com>
2024-07-22 11:23:45 +0200

    change test name

--- tests/fibonacci_test.py
+++ tests/fibonacci_test.py
@@ -1,6 +1,6 @@
 from src.utils import fibonacci
 
-def test_sequence_pass():
+def test_sequence():
     expected = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
     result = fibonacci(10)
     assert result == expected
